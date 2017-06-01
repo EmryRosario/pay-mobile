65dfb90f335bc34d4b14517f05ac09e84975cc56
Emry Rosario <emry3008@gmail.com>
2017-06-01 15:07:18 -0400

    Added concept description in proof

--- client/src/proof/containers/content.jsx
+++ client/src/proof/containers/content.jsx
@@ -16,12 +16,10 @@ class Content extends Component {
 
     let cuota = this.props.proof['REC_MTOCOB'] > mora ? this.props.proof['REC_MTOCOB'] - mora : 0
 
-    this.props.proof['REC_DESCRI'] = this.props.proof['REC_DESCRI'] || '...................'
-
     return (<div id={`proof-content-${this.props.proof['REC_NUMERO']}`} className={'col-xs-12  no-padding-right no-padding-left'}>
       <div className={'col-xs-12 text-center no-padding-left'}>
         <div className={'col-xs-6 text-left'}><span>{`CONCEPTO...: `}</span></div>
-        <div className={'col-xs-6 text-left'}><span>{'PAGO POR DESEMBOLSO'}</span></div>
+        <div className={'col-xs-6 text-left'}><span>{this.props.proof['REC_DESCRI']}</span></div>
         <div className={'col-xs-6 text-left'}><span>{`CUOTA...: `}</span></div>
         <div className={'col-xs-6 text-left'}><span>
           {numeral(cuota).format('$ 0,0.00')}
