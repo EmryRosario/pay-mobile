f1c9f5fce735286dcb742e7567e0e8ee56a3f8f0
Emry Rosario <emry3008@gmail.com>
2017-06-01 13:53:55 -0400

    Added validation show payment not applied in the proofs

--- client/src/proof/containers/content.jsx
+++ client/src/proof/containers/content.jsx
@@ -10,6 +10,12 @@ class Content extends Component {
     let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
     let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
 
+    let mora = (this.props.proof['REC_MTOCOB'] - this.props.proof['REC_MORA']) >= 0
+    ? this.props.proof['REC_MORA']
+    : this.props.proof['REC_MTOCOB']
+
+    let cuota = this.props.proof['REC_MTOCOB'] > mora ? this.props.proof['REC_MTOCOB'] - mora : 0
+
     this.props.proof['REC_DESCRI'] = this.props.proof['REC_DESCRI'] || '...................'
 
     return (<div id={`proof-content-${this.props.proof['REC_NUMERO']}`} className={'col-xs-12  no-padding-right no-padding-left'}>
@@ -17,9 +23,13 @@ class Content extends Component {
         <div className={'col-xs-6 text-left'}><span>{`CONCEPTO...: `}</span></div>
         <div className={'col-xs-6 text-left'}><span>{'PAGO POR DESEMBOLSO'}</span></div>
         <div className={'col-xs-6 text-left'}><span>{`CUOTA...: `}</span></div>
-        <div className={'col-xs-6 text-left'}><span>{numeral(this.props.proof['REC_MTOCUO']).format('$ 0,0.00')}</span></div>
+        <div className={'col-xs-6 text-left'}><span>
+          {numeral(cuota).format('$ 0,0.00')}
+        </span></div>
         <div className={'col-xs-6 text-left'}><span>{`MORA...: `}</span></div>
-        <div className={'col-xs-6 text-left'}><span>{numeral(this.props.proof['REC_MORA']).format('$ 0,0.00')}</span></div>
+        <div className={'col-xs-6 text-left'}><span>{
+          numeral(mora).format('$ 0,0.00')
+          }</span></div>
         <div className={'col-xs-6 text-left'}><span>{`OTROS ING...: `}</span></div>
         <div className={'col-xs-6 text-left'}><span>{numeral(this.props.proof['REC_INGRES']).format('$ 0,0.00')}</span></div>
         <div className={'col-xs-6 text-left'}><span>{`DESCUENTO...: `}</span></div>
@@ -27,10 +37,7 @@ class Content extends Component {
         <div className={'col-xs-4 col-xs-offset-6 text-left border-bottom'} />
         <div className={'col-xs-6 text-left'}><span>{'TOTAL.....:'}</span></div>
         <div className={'col-xs-6 text-left'}><span>
-          {numeral(this.props.proof['REC_MTOCUO'] +
-           this.props.proof['REC_MORA'] +
-           this.props.proof['REC_INGRES'] +
-            this.props.proof['REC_DESCUE']).format('$ 0,0.00')}
+          {numeral(this.props.proof['REC_MTOCOB']).format('$ 0,0.00')}
         </span></div>
         <div className={'col-xs-12 border-bottom'} />
         <div className={'col-xs-6 text-left'}>{'IMPRESO POR:'}</div>
--- client/src/proof/proof.jsx
+++ client/src/proof/proof.jsx
@@ -8,6 +8,9 @@ class Proof extends Component {
     window.print()
   }
   render () {
+    if (this.props.proof['REC_ESTADO'] !== '3') {
+      return (<div className={'jumbotron col-xs-8 col-x-offset-2'}><h4>{'No se ha aplicado ningun pago.'}</h4></div>)
+    }
     return (<div className={'col-xs-12 no-padding-left  no-padding-right'} id={`proof-${this.props.proof['REC_NUMERO']}`}>
       <Header proof={this.props.proof} company={this.props.company} client={this.props.client} />
       <Content proof={this.props.proof} user={this.props.user} />
